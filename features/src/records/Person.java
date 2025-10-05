d4d686356bd9f83a49026813907643543166e0a5
Bruno Marques <brunomcamilo.br@gmail.com>
2025-10-05 13:23:15 -0300

    Added validations in compact constructor

--- features/src/records/Person.java
+++ features/src/records/Person.java
@@ -1,3 +1,11 @@
 package records;
 
-public record Person(String nome, String email, int age) { }
+public record Person(String nome, String email, int age) {
+
+    public Person {
+        // validations in compact constructor
+        if(age < 0) throw new IllegalArgumentException("Age cannot be negative");
+        if(email == null || !email.contains("@")) throw new IllegalArgumentException("Invalid email");
+    }
+
+}
