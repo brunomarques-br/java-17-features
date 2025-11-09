85797d1e46f3a74fef2fc42416385922fbafe973
Bruno Marques <brunomcamilo.br@gmail.com>
2025-11-09 09:05:13 -0300

    Records description added in javadoc

--- features/src/records/Person.java
+++ features/src/records/Person.java
@@ -1,5 +1,18 @@
 package records;
 
+
+/**
+ * Description: Records (immutable data)
+ * When to use it?
+ * -> DTOs
+ * -> API responses
+ * -> Value Objects
+ * -> Pure data classes
+ *
+ * @param nome
+ * @param email
+ * @param age
+ */
 public record Person(String nome, String email, int age) {
 
     public Person {
